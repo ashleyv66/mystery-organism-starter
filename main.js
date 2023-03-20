95192f491ed469e5df6ee60e6e0db2572365e95e
ashleyv66 <121967070+ashleyv66@users.noreply.github.com>
2023-03-20 11:47:20 +0000

    Update main.js
    Updated text notes in to improve legibility

--- main.js
+++ main.js
@@ -13,10 +13,13 @@ const mockUpStrand = () => {
   return newStrand;
 };
 // My code starts here
+//Factory function to generate new organisms
 const pAequorFactory = (orgNo, dna) => {
   return {
     specimenNum: orgNo,
     dna: dna,
+    //methods
+    //mutates the DNA of the organism by randomly changing a base for another
     mutate() {
       let newBase = returnRandBase()
       let rndIndex = Math.floor(Math.random() * 15);
@@ -25,6 +28,7 @@ const pAequorFactory = (orgNo, dna) => {
       this.dna[rndIndex] = newBase;
       console.log(this.dna[rndIndex]);}
     },
+    //Performs a comparison of two organisms to see how much DNA they have in common. Logs the % in common to console.
     compareDNA(newStrain) {
       let curDNA = this.dna
       let newDna = newStrain.dna
@@ -40,6 +44,7 @@ const pAequorFactory = (orgNo, dna) => {
       let percent = parseFloat((counter/curDNA.length)*100).toFixed(1);
     console.log(`Specimen ${ex1.specimenNum} and specimen ${ex2.specimenNum} have ${percent}% DNA in common.`)
     },
+    //Evaluates the DNA strand of the organism to see if it is likely to survive based on the % of C and G bases in the strand. Evaluates to a boolean.
     willLikelySurvive() {
       let sDna = this.dna;
       let counter2 = 0;
@@ -48,10 +53,10 @@ const pAequorFactory = (orgNo, dna) => {
           counter2++;
         }
       }
-      //console.log(counter2);
-      //console.log(sDna.length);
+      //console.log(counter2);// test log
+      //console.log(sDna.length);// test log
       let likelyhood = 100 * counter2/sDna.length;
-      //console.log(likelyhood);
+      //console.log(likelyhood); //test log
       if (likelyhood >= 60) {
         return true;
       }
@@ -60,6 +65,7 @@ const pAequorFactory = (orgNo, dna) => {
     
   }
 }
+//Generates 30 instances of the organism which are likely to survive
 const instances = []
 for (let inst = 0; instances.length < 30; inst++) {
   let strand = pAequorFactory(inst,mockUpStrand());
@@ -71,14 +77,9 @@ for (let inst = 0; instances.length < 30; inst++) {
   }
 }
 console.log(instances)
-//console.log(instances.length)
-//console.log(instances[1].dna)
-
-
-
-
-
-
+//console.log(instances.length)//test log
+//console.log(instances[1].dna)//test log
+/* Various test logs
 //const ex1 = pAequorFactory(1,mockUpStrand());
 //const ex2 = pAequorFactory(2,mockUpStrand());
 //console.log(ex1.specimenNum);
@@ -87,7 +88,7 @@ console.log(instances)
 //ex2.compareDNA(ex1);
 //console.log(ex2.willLikelySurvive())
 //console.log(ex2.dna);
-
+*/ 
 
 
 
